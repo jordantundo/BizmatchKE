58de8772eb9cdfeca1b831983040beb2cafe91cc
jotham <48428834+jordantundo@users.noreply.github.com>
2025-05-16 14:34:22 +0300

    Update eslint.config.cjs

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,19 +1,26 @@
 const next = require('@next/eslint-plugin-next');
 
-/** @type {import('eslint').FlatConfig[]} */
 module.exports = [
   {
     files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {
       '@next/next': next,
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
     },
     languageOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: './tsconfig.json', // optional but recommended if you have tsconfig
+      },
     },
     rules: {
       ...next.configs['core-web-vitals'].rules,
-      'react/no-unescaped-entities': 'off', // Disable that rule
+      'react/no-unescaped-entities': 'off',
     },
   },
 ];
